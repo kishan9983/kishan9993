0f6c433b03c423cf6c753a43aa72275d16756acf
kishan9983 <patelkishan9390@gmail.com>
2025-09-10 20:37:00 +0530

    Add files via upload

---
+++ Works/9-9-25/Kishan Prac-15.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int i,num;
+  
+  printf("Enter Number");
+  scanf("%d",&num);
+  
+  for(i=1;i<=10;i++)
+  {
+    int ans = num*i;
+    printf("\n %d * %d = %d",num,i,ans);
+  }
+ getch();
+}
