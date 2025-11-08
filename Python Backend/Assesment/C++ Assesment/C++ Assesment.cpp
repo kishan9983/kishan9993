3a80733042904381b385e372bbf408aa1ec16c8a
kishan9983 <patelkishan9390@gmail.com>
2025-11-08 10:06:01 +0530

    main

--- Python Backend/Assesment/C++ Assesment/C++ Assesment.cpp
+++ Python Backend/Assesment/C++ Assesment/C++ Assesment.cpp
@@ -4,18 +4,17 @@ using namespace std;
 class Lecture {
 private:
     char lecturerName[50];
-    char subjectName[50];
     char courseName[50];
     int numberOfLectures;
-
+    int subjectCode;  
 public:
     Lecture() { }
 
     void assignLectureDetails() {
         cout << "Enter Lecturer Name: ";
         cin >> lecturerName;
-        cout << "Enter Subject Name: ";
-        cin >> subjectName;
+        cout << "Enter Subject Code (Number Only): ";
+        cin >> subjectCode;
         cout << "Enter Course Name: ";
         cin >> courseName;
         cout << "Enter Number of Lectures: ";
@@ -24,25 +23,15 @@ public:
 
     void displayLectureDetails() {
         cout << "\nLecturer Name: " << lecturerName;
-        cout << "\nSubject Name: " << subjectName;
+        cout << "\nSubject Code: " << subjectCode;
         cout << "\nCourse Name: " << courseName;
         cout << "\nNumber of Lectures: " << numberOfLectures << "\n";
     }
 
-    char* getSubjectName() {
-        return subjectName;
+    int getSubjectCode() {
+        return subjectCode;
     }
 };
-
-int compareStrings(char a[], char b[]) {
-    int i = 0;
-    while(a[i] != '\0' && b[i] != '\0') {
-        if(a[i] != b[i])
-            return 0;
-        i++;
-    }
-    return (a[i] == '\0' && b[i] == '\0');
-}
 
 int main() {
     Lecture lecturers[5];
@@ -53,28 +42,23 @@ int main() {
     }
 
     while(true) {
-        char searchSubject[50];
-        int found = 0;
+        int searchCode;
         int choice;
 
-        cout << "\nPress 1 to Search by Subject or 0 to Exit: ";
+        cout << "\nPress 1 to Search by Subject Code or 0 to Exit: ";
         cin >> choice;
 
         if(choice == 0)
             break;
 
-        cout << "Enter Subject Name: ";
-        cin >> searchSubject;
+        cout << "Enter Subject Code: ";
+        cin >> searchCode;
 
         for(int i = 0; i < 5; i++) {
-            if(compareStrings(lecturers[i].getSubjectName(), searchSubject)) {
+            if(lecturers[i].getSubjectCode() == searchCode) {
                 lecturers[i].displayLectureDetails();
-                found = 1;
             }
         }
-
-        if(found == 0)
-            cout << "\nNo lecturer found for this subject.\n";
     }
 
     return 0;
